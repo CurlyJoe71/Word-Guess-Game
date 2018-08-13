26728d1530840403998359cbdd20b56ebf90314a
Curly Joe <curlyjoe71@gmail.com>
2018-08-13 15:08:38 -0700

    Updated files for homework.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,24 @@
 // declaring variables
 var wordList = ['tardis', 'companion', 'doctor', 'dalek', 'cyberman', 'silence', 'universe', 'travel', 'hero', 'time', 'history', 'future'];
-var secretWord = function() {
-    return wordList[(Math.floor(Math.random() * wordList.length))];
+var gameBoard = [];
+
+// function to get random word and convert to array;
+var secretWordFunction = function() {
+    var secretWordArray = [];
+    var secretWord = wordList[(Math.floor(Math.random() * wordList.length))];
+    secretWordArray = secretWord.split('');
+    gameBoard = secretWordArray;
+    console.log(gameBoard);
+}
+
+document.onkeyup = function(event) {
+    // secretWordFunction();
+    var userGuess = event.key;
+    //function to check guess with each letter
+    gameBoard.forEach(function(value) {
+        if (userGuess === value) {
+            console.log(value);
+        }    
+        });
+    
 }
