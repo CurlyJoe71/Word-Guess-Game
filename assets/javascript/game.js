1354343ee75c0c17ce7ea2db1a3dd6d45b592e85
Curly Joe <curlyjoe71@gmail.com>
2018-08-13 18:37:31 -0700

    updated hangman game javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,38 @@
 // declaring variables
 var wordList = ['tardis', 'companion', 'doctor', 'dalek', 'cyberman', 'silence', 'universe', 'travel', 'hero', 'time', 'history', 'future'];
-var gameBoard = [];
 
+var answer = [];
 // function to get random word and convert to array;
-var secretWordFunction = function() {
+secretWordFunction = function() {
     var secretWordArray = [];
     var secretWord = wordList[(Math.floor(Math.random() * wordList.length))];
     secretWordArray = secretWord.split('');
-    gameBoard = secretWordArray;
-    console.log(gameBoard);
+    answer = secretWordArray;
+    console.log(answer);
 }
+secretWordFunction();
+
+var gameBoard = [];
+console.log(gameBoard);
+for (let i = 0; i < answer.length; i++) {
+    gameBoard[i] = "_";
+}
+console.log(gameBoard);
+
+//am I going to need some sort of indexOf to match the position of the userGuess and something else...
+
 
 document.onkeyup = function(event) {
     // secretWordFunction();
     var userGuess = event.key;
     //function to check guess with each letter
-    gameBoard.forEach(function(value) {
+    answer.forEach(function(value) {
         if (userGuess === value) {
+            var location = answer.indexOf(value);
             console.log(value);
+            console.log(location);
+            gameBoard[location] = answer[location];
+            console.log(gameBoard);
         }    
         });
     
